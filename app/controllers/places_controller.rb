6e2531ad8b91e78500c6726b219a6b87372d6698
jonny phun <jonny_phun@hotmail.com>
2019-04-24 04:35:17 +0000

    new page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,16 +7,22 @@ class PlacesController < ApplicationController
     @places = Place.all
 
 
-
-
-
-
   end
 
 
   def index
-   @places = Place.paginate(page: params[:page])
+
+    @places = Place.paginate(page: params[:page], per_page: 2)
+    
   end
+
+
+  def new
+    @place = Place.new
+  end
+
+
+
 
 
 end
