58a8cd706eaff04ba95dd0afefc6282b37e1c952
jonny phun <jonny_phun@hotmail.com>
2019-04-26 17:04:52 +0000

    validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,9 +25,15 @@ class PlacesController < ApplicationController
 
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
-  end
+    @place = current_user.places.create(place_params)
+
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  
+   end
 
   def show
     @place = Place.find(params[:id])
@@ -52,7 +58,11 @@ class PlacesController < ApplicationController
 
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
