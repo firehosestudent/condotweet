01af7e1d5babac25f63adef715b5c21893f3d384
jonny phun <jonny_phun@hotmail.com>
2019-05-02 17:22:53 +0000

    message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,23 +3,21 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'condotweet.herokuapp.com' }
 
   config.action_mailer.perform_deliveries = true 
 
-  config.action_mailer.raise_delivery_errors = true 
-
-  config.action_mailer.default :charset => "utf-8"
+  
+  
 
 
   ActionMailer::Base.smtp_settings = {
-    :from => 'jonny_phun@hotmail.com',
+    
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'http://condotweet.heroku.com',
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
 
