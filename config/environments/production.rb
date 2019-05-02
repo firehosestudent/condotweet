51bd1d54ce5614ae17f56c32f820b37eda0d37ca
jonny phun <jonny_phun@hotmail.com>
2019-05-02 01:26:43 +0000

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
