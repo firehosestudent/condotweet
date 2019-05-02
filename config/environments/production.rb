c7f004f8d34153e21f58df16c27a01d5d756bcce
jonny phun <jonny_phun@hotmail.com>
2019-05-02 04:48:26 +0000

    message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { :host => 'condotweet.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { :host => 'condotweet.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
