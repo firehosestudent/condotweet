2c8707188e8aefcc39b9b6ff392859a1ffb5eea9
jonny phun <jonny_phun@hotmail.com>
2019-05-02 02:12:04 +0000

    dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,17 +2,20 @@ Rails.application.configure do
 
   
 
-   #config.action_mailer.delivery_method = :smtp
+   config.action_mailer.delivery_method = :smtp
 
-  #config.action_mailer.smtp_settings = {
-   # address: "smtp.gmail.com",
-    #port: 587,
-    #domain: "example.com",
-    #authentication: 'plain',
-    #enable_starttls_auto: true,
-    #user_name: ENV['GMAIL_ADDRESS'],
-    #password: ENV['GMAIL_PASSWORD']
-  #}
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
+
+  config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -66,6 +69,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+  
 
 end
