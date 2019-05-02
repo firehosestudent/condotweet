7b308fe0495d3718900c3154c7a0d2fdba8dfadb
jonny phun <jonny_phun@hotmail.com>
2019-05-02 04:53:08 +0000

    test10

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,7 +20,7 @@ class Comment < ApplicationRecord
   end
 
   def send_comment_email
-    NotificationMailer.comment_added(self).deliver.now
+    NotificationMailer.comment_added(self).deliver_now
   end
 
 end
